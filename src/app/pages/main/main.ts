c22ebff3528236396873aec9ce25fc11af3dc5aa
sofit-ta <118261369+sofit-ta@users.noreply.github.com>
2026-03-06 13:06:12 +0300

    fixed routing problem

--- src/app/pages/main/main.ts
+++ src/app/pages/main/main.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
